f985a83cd203ad66d3c27fbd663c65ffd729be12
kaminora <kaminoura9207@gmail.com>
2019-10-21 01:55:49 +0900

    Fix playbook to use mh-z19

--- roles/co2-detection/files/get-concentration.sh
+++ roles/co2-detection/files/get-concentration.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-value=$(sudo python ~/mh_z19/mh_z19.py)
+value=$(sudo python -m ~/mh_z19/mh_z19.py)
 curl -i -XPOST "http://localhost:8086/write?db=home" --data-binary "co2, field1=${value}"
