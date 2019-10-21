1c18669e1c46aad36c5edeeb70a78581fb34a53b
kaminora <kaminoura9207@gmail.com>
2019-10-21 22:05:51 +0900

    Fix error to save data

--- roles/co2-detection/files/get-concentration.sh
+++ roles/co2-detection/files/get-concentration.sh
@@ -2,4 +2,5 @@
 
 co2_value=$(sudo python -m mh_z19 --all | jq .co2)
 temperature_value=$(sudo python -m mh_z19 --all | jq .temperature)
-curl -i -XPOST "http://localhost:8086/write?db=home" --data-binary "co2, value=${co2_value} temperature, value=${temperature_value}"
+curl -i -XPOST "http://localhost:8086/write?db=home" --data-binary "co2,host=server01 value=${co2_value}
+temperature,host=server01 value=${temperature_value}"
